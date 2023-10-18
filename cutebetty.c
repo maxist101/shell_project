ffaa5b9da8d0891e7fedad775ef6bcde2010166d
maxist101 <netochukwu26@gmail.com>
2023-10-18 14:16:47 -0700

    betty

---
+++ cutebetty.c
@@ -0,0 +1,17 @@
+#include "shell.h"
+
+/**
+* main - void an entry to the programe function.
+* a code that passes betty checks.
+* Return: 0 on success alwa.
+*/
+
+int main(void)
+{
+char *aux = "Jessica and Max\n";
+ssize_t length = 16;
+
+/*using the write function for our betty check*/
+write(STDOUT_FILENO, aux, length);
+return (0);
+}
