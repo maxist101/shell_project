4e9320e5addc93d6a22f4413492a9ca4dc0e7c0f
jessyovia <jessyovia100@gmail.com>
2023-10-18 18:11:50 -0700

    mint

--- cutebetty.c
+++ cutebetty.c
@@ -1,7 +1,7 @@
 #include "shell.h"
 
 /**
-* main - void an entry to the programe function.
+* mint - void an entry to the programe function.
 * a code that passes betty checks.
 * Return: 0 on success alwa.
 */
