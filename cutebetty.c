9e68545900c0b7972bed03c1ad772578666481cd
maxist101 <netochukwu26@gmail.com>
2023-10-19 13:01:52 -0700

    dd

--- cutebetty.c
+++ cutebetty.c
@@ -1,12 +1,12 @@
 #include "shell.h"
 
 /**
-* main - void an entry to the programe function.
+* mainn - void an entry to the programe function.
 * a code that passes betty checks.
 * Return: 0 on success alwa.
 */
 
-int main(void)
+int mainn(void)
 {
 char *aux = "Jessica and Max\n";
 ssize_t length = 16;
