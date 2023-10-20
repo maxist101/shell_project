4d61721687fa8aad1c377e7289a3b0e7aac26549
jessyovia <jessyovia100@gmail.com>
2023-10-20 15:21:57 -0700

    our main c file

--- shell.h
+++ shell.h
@@ -8,6 +8,16 @@
 #include <stdlib.h>
 #include <string.h>
 #include <sys/wait.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <string.h>
+#include <stdbool.h>
+#include <sys/stat.h>
+
+#define BUFFER_SIZE 256
 
 
 
