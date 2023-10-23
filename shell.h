ba39b5ac4bb9e8f91a05764eefdfb19a4014cace
maxist101 <netochukwu26@gmail.com>
2023-10-23 15:34:07 -0700

    ALX IS FUN

--- shell.h
+++ shell.h
@@ -1,27 +1,27 @@
 #ifndef SHELL_H
 #define SHELL_H
 
-/* starting point of all our team project #include files*/
-#include <stdio.h>
-#include <unistd.h>
-#include <sys/stat.h>
-#include <stdlib.h>
-#include <string.h>
-#include <sys/wait.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <sys/types.h>
-#include <sys/wait.h>
 #include <string.h>
-#include <stdbool.h>
+#include <sys/wait.h>
 #include <sys/stat.h>
+#include <errno.h>
+#include <stddef.h>
 
-#define BUFFER_SIZE 256
-
-/* starting point of all our prototypes, see Man in terminal*/
-void exe_cmd(char *cmd, char *argv[]);
-
+#define MAXIMUM_LINE 1024
 
 
+int main(int argc, char **argv);
+
+int set_environment(char **tokz);
+int unset_environment(char **tokz);
+int built_in(char **tokz_array);
+extern char **environ;
+ssize_t echo_cmd(char **ln, size_t *neto);
+void str_spl(char *string);
+int suenv(void);
+void exe(char **tokz);
+char *pathfinder(const char *locate);
 #endif
